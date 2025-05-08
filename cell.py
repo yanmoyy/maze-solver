bf21b665cfbee15c7e5a201dfa50feeba0607320
yanmo <jrsky723@gmail.com>
2025-05-08 13:21:50 +0900

    fix:change x,y axis and test

--- cell.py
+++ cell.py
@@ -2,23 +2,24 @@ from graphics import Line, Point, Window
 
 
 class Cell:
-    def __init__(self, win: Window) -> None:
+    def __init__(self, win) -> None:
         self._x1 = None
         self._x2 = None
         self._y1 = None
         self._y2 = None
-        self._win = win
+        self._win: Window | None = win
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
 
     def draw(self, x1: float, y1: float, x2: float, y2: float):
+        if self._win is None:
+            return
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
         self._y2 = y2
-
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
             self._win.draw_line(line)
@@ -40,6 +41,8 @@ class Cell:
         return Point(center_x, center_y)
 
     def draw_move(self, to_cell: "Cell", undo=False):
+        if self._win is None:
+            return
         center_1 = self.get_center()
         center_2 = to_cell.get_center()
         line = Line(center_1, center_2)
--- maze.py
+++ maze.py
@@ -14,7 +14,7 @@ class Maze:
         num_cols,
         cell_size_x,
         cell_size_y,
-        win,
+        win=None,
     ) -> None:
         self._x1 = x1
         self._y1 = y1
@@ -22,29 +22,30 @@ class Maze:
         self._num_cols = num_cols
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
-        self._win: Window = win
+        self._win: Window | None = win
         self._cells: List[List[Cell]] = []
 
         self._create_cells()
 
     def _create_cells(self) -> None:
-        for i in range(self._num_rows):
+        for i in range(self._num_cols):
             col_cells = []
-            for j in range(self._num_cols):
+            for j in range(self._num_rows):
                 col_cells.append(Cell(self._win))
             self._cells.append(col_cells)
-        for i in range(self._num_rows):
-            for j in range(self._num_cols):
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
                 self._draw_cell(i, j)
 
     def _draw_cell(self, i: int, j: int):
-        x1 = self._cell_size_x * i
-        y1 = self._cell_size_y * j
+        x1 = self._x1 + self._cell_size_x * i
+        y1 = self._y1 + self._cell_size_y * j
         x2 = x1 + self._cell_size_x
         y2 = y1 + self._cell_size_y
         self._cells[i][j].draw(x1, y1, x2, y2)
         self._animate()
 
     def _animate(self):
-        self._win.redraw()
-        time.sleep(0.05)
+        if self._win is not None:
+            self._win.redraw()
+            time.sleep(0.05)
