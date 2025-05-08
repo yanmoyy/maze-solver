7da5e544c8258f8e2dbddcabd0749f58e3d1a865
yanmo <jrsky723@gmail.com>
2025-05-08 11:25:19 +0900

    feat:draw cell

---
+++ cell.py
@@ -0,0 +1,29 @@
+from graphics import Line, Point, Window
+
+
+class Cell:
+    def __init__(self, x1: float, y1: float, x2: float, y2: float, win: Window) -> None:
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        self._win = win
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+
+    def draw(self):
+        x1, x2, y1, y2 = self._x1, self._x2, self._y1, self._y2
+        if self.has_left_wall:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line)
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line)
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line)
+        if self.has_bottom_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line)
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+from cell import Cell
 from graphics import Line, Point, Window
 
 
 def main():
     win = Window(800, 600)
-    line = Line(Point(1, 1), Point(100, 100))
-    win.draw_line(line, "red")
+    cell = Cell(50, 50, 100, 100, win)
+    cell.draw()
     win.wait_for_close()
 
 
